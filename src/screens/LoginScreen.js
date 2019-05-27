a0e3bc4eb4831feb98638d0b2c6e9b4b313ea5be
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-27 16:49:01 +0200

    Create layout

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,22 +1,75 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  StatusBar
+} from "react-native";
+import Login from "../components/Login";
 
 export default class LoginScreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text> LoginScreen </Text>
-        <Text style={styles.text} onPress={() => navigate("Home")}>
-          CLICK
-        </Text>
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#7070EF" />
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../images/cinema.svg.png")}
+          />
+        </View>
+        <Login />
+        <View style={styles.formContainer}>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => navigate("Home")}
+          >
+            <Text style={styles.buttonText}> Login </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.logoContainer}>
+          <Text
+            style={{ color: "blue", textAlign: "center" }}
+            onPress={() => navigate("SignUp")}
+          >
+            Dont't have account ? Click here to registry.
+          </Text>
+        </View>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  logoContainer: {
+    alignItems: "center",
+    flexGrow: 2,
+    justifyContent: "center"
+  },
+  logo: {
+    width: "auto",
+    height: "auto"
+  },
+  buttonContainer: {
+    backgroundColor: "#7070EF",
+    paddingVertical: 15,
+    marginBottom: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    borderWidth: 0,
+    borderColor: "transparent",
+    borderRadius: 12,
+    padding: 90
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 20
   }
 });
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,12 +1,68 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar
+} from "react-native";
+import SignUpForm from "../components/SignUpForm";
 
 export default class SignUpScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> SignUpScreen </Text>
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#7070EF" barStyle="light-content" />
+        <View style={styles.logoContainer} />
+        <View style={styles.formContainer}>
+          <SignUpForm />
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => this.props.navigation.goBack()}
+          >
+            <Text style={styles.buttonText}> Submit </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  logoContainer: {
+    alignItems: "center",
+    flexGrow: 1,
+    justifyContent: "center",
+    marginTop: 52
+  },
+  formContainer: {
+    alignItems: "center",
+    flexGrow: 4,
+    justifyContent: "center",
+    marginBottom: 25
+  },
+  logo: {
+    width: 200,
+    height: 100
+  },
+  buttonContainer: {
+    backgroundColor: "#7070EF",
+    paddingVertical: 15,
+    marginBottom: 20,
+    marginLeft: 50,
+    marginRight: 50,
+    borderWidth: 0,
+    borderColor: "transparent",
+    borderRadius: 12,
+    padding: 90
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "black",
+    fontSize: 20
+  }
+});
