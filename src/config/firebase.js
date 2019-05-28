1262f686bb0d3a2484e518e54b8c9ce369e5a44f
Robert Lesniak <robert.lesniak@thecogworks.com>
2019-05-28 11:05:44 +0200

    added firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var config = {
+  apiKey: "AIzaSyDWdmIxG3RwAMli1s-GhAIib5du0NfyCaw",
+  authDomain: "cinemapp-service.firebaseapp.com",
+  databaseURL: "https://cinemapp-service.firebaseio.com",
+  projectId: "cinemapp-service",
+  storageBucket: "cinemapp-service.appspot.com",
+  messagingSenderId: "66468469024"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+export const auth = firebase.auth
+export const provider = new firebase.auth.FacebookAuthProvider()
+
+export default firebase;
