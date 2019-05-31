678c6f99c63a9cfa7f39e9263b145e448ecd6978
Robert Lesniak <robert.lesniak@thecogworks.com>
2019-05-31 17:56:01 +0200

    added search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import firebase from "../../config/firebase";
 
 export default class SearchScreen extends Component {
+
+  componentDidMount() {
+    const markers = [];
+    const uids2 = [];
+    firebase
+      .firestore()
+      .collection("films")
+      .get()
+      .then(querySnapshot => {
+        console.log(querySnapshot);
+        querySnapshot.docs.forEach(doc => {
+          markers.push(doc.data());
+        });
+        this.setState({ movies: markers });
+        this.setState({ uids: uids2 });
+        // console.log(this.state.movies[0]);
+        /// console.log(this.state.movies[0].uid);
+      });
+  }   
+
+
   render() {
     return (
       <View>
-        <Text> SearchScreen </Text>
+        <Text> {console.log(movie)} </Text>
       </View>
     );
   }
