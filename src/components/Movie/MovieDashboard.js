8fba377a56d066ee2a9aeda8b00ff3e0dea508c7
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-31 18:56:57 +0200

    list movies with popups

--- src/components/Movie/MovieDashboard.js
+++ src/components/Movie/MovieDashboard.js
@@ -4,7 +4,8 @@ import firebase from "../../config/firebase";
 import { getTheme } from "react-native-material-kit";
 import Dialog, {
   DialogContent,
-  SlideAnimation
+  SlideAnimation,
+  DialogTitle
 } from "react-native-popup-dialog";
 
 class MovieDashboard extends Component {
@@ -39,6 +40,7 @@ class MovieDashboard extends Component {
 
   render() {
     const theme = getTheme();
+
     return (
       <View>
         <ScrollView style={{ marginBottom: 5 }}>
@@ -47,6 +49,11 @@ class MovieDashboard extends Component {
             : this.state.movies.map(movie => {
                 return (
                   <View key={movie.movieApiId}>
+                    {console.log(
+                      movie.trailerURL.substring(
+                        movie.trailerURL.indexOf("=") + 1
+                      )
+                    )}
                     <View
                       style={{
                         alignItems: "center",
@@ -70,6 +77,67 @@ class MovieDashboard extends Component {
                           borderRadius: 10
                         }}
                       />
+                      <View>
+                        <Dialog
+                          visible={this.state.visible}
+                          dialogAnimation={
+                            new SlideAnimation({
+                              slideFrom: "bottom"
+                            })
+                          }
+                          dialogTitle={<DialogTitle title={movie.title} />}
+                          onTouchOutside={() => {
+                            this.setState({ visible: false });
+                          }}
+                          style={{ backgroundColor: "transparent" }}
+                        >
+                          <DialogContent
+                            style={{ backgroundColor: "transparent" }}
+                          >
+                            <Text style={{ textAlign: "center" }}>
+                              Director: {movie.director}
+                            </Text>
+                            <Text style={{ textAlign: "center" }}>
+                              Actors: {movie.actors}
+                            </Text>
+                            <Text style={{ textAlign: "center" }}>
+                              Duration: {movie.duration} min
+                            </Text>
+                            <Text style={{ textAlign: "center" }}>
+                              Type: {movie.type}
+                            </Text>
+                            <Image
+                              source={{ uri: movie.image }}
+                              style={{
+                                width: 200,
+                                height: 280,
+                                alignContent: "center",
+                                justifyContent: "center",
+                                marginTop: 2,
+                                borderRadius: 10,
+                                marginLeft: 17
+                              }}
+                            />
+                            <TouchableOpacity
+                              style={{
+                                alignItems: "center",
+                                backgroundColor: "#DDDDDD",
+                                padding: 10,
+                                marginBottom: 2,
+                                borderRadius: 20,
+                                backgroundColor: "#0051A5",
+                                marginTop: 5
+                              }}
+                            >
+                              <Text
+                                style={{ color: "white", fontWeight: "bold" }}
+                              >
+                                Avaiability
+                              </Text>
+                            </TouchableOpacity>
+                          </DialogContent>
+                        </Dialog>
+                      </View>
 
                       <Text style={{ fontWeight: "bold", fontSize: 18 }}>
                         {movie.title} ({movie.Year})
