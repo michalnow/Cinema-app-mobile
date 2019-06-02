272da6ee5cb30ea0799108043414213db653fe60
Michał Nowakowski <michal951@poczta.onet.pl>
2019-06-02 19:35:17 +0200

    small changes

--- src/screens/HomeMovies.js
+++ src/screens/HomeMovies.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
-import firebase from "../config/firebase"
+import React, { Component } from "react";
+import firebase from "../config/firebase";
 import { Text, View, Image, ActivityIndicator, ScrollView } from "react-native";
 import { createMaterialTopTabNavigator } from "react-navigation";
-import { Divider, Button, Card, Paragraph } from 'react-native-paper';
+import { Divider, Button, Card, Paragraph } from "react-native-paper";
 import Icon from "react-native-vector-icons/Ionicons";
 import { withNavigation } from "react-navigation";
 
 class HomeMovies extends Component {
-
   state = {
     movie: null
-  }
+  };
 
   componentDidMount() {
     const markers = [];
@@ -24,7 +23,7 @@ class HomeMovies extends Component {
           markers.push(doc.data());
         });
         this.setState({ movie: markers });
-        console.log(this.state.movie)
+        console.log(this.state.movie);
       });
   }
 
@@ -35,7 +34,7 @@ class HomeMovies extends Component {
           style={{ marginTop: 10 }}
           source={require("../images/cinema1.png")}
         />
-          <Text
+        <Text
           style={{
             marginTop: 15,
             fontSize: 40,
@@ -45,60 +44,65 @@ class HomeMovies extends Component {
         >
           Recently added
         </Text>
-        
-          {this.state.movie == null ? (
+
+        {this.state.movie == null ? (
           <ActivityIndicator
             style={{
               flex: 1,
               justifyContent: "center",
               flexDirection: "row",
-              justifyContent: "space-around",
+              justifyContent: "space-around"
             }}
             size="large"
             color="#0000ff"
           />
         ) : (
           <ScrollView>
-              <Card>
-                <Card.Title title={this.state.movie[0].title} />
-                <Card.Cover
-                  source={{ uri: this.state.movie[0].image }}
-                />
-                <Card.Content>
-                  <Paragraph>{this.state.movie[0].plot}</Paragraph>
-                </Card.Content>
-                <Card.Actions>
-                  <Button onPress={() => this.props.navigation.navigate("Movie0Av")}>Movie info</Button>
-                </Card.Actions>
+            <Card>
+              <Card.Title title={this.state.movie[0].title} />
+              <Card.Cover source={{ uri: this.state.movie[0].image }} />
+              <Card.Content>
+                <Paragraph>{this.state.movie[0].plot}</Paragraph>
+              </Card.Content>
+              <Card.Actions>
+                <Button
+                  onPress={() => this.props.navigation.navigate("Movie0Av")}
+                >
+                  Movie info
+                </Button>
+              </Card.Actions>
               <Divider />
-                <Card.Title title={this.state.movie[1].title} />
-                <Card.Cover
-                  source={{ uri: this.state.movie[1].image }}
-                />
-                <Card.Content>
-                  <Paragraph>{this.state.movie[1].plot}</Paragraph>
-                </Card.Content>
-                <Card.Actions>
-                  <Button onPress={() => this.props.navigation.navigate("Movie1Av")}>Movie info</Button>
-                </Card.Actions>
-                <Divider />
-                <Card.Title title={this.state.movie[2].title} />
-                <Card.Cover
-                  source={{ uri: this.state.movie[2].image }}
-                />
-                <Card.Content>
-                  <Paragraph>{this.state.movie[2].plot}</Paragraph>
-                </Card.Content>
-                <Card.Actions>
-                  <Button onPress={() => this.props.navigation.navigate("Movie2Av")}>Movie info</Button>
-                </Card.Actions>
-              </Card>
+              <Card.Title title={this.state.movie[1].title} />
+              <Card.Cover source={{ uri: this.state.movie[1].image }} />
+              <Card.Content>
+                <Paragraph>{this.state.movie[1].plot}</Paragraph>
+              </Card.Content>
+              <Card.Actions>
+                <Button
+                  onPress={() => this.props.navigation.navigate("Movie1Av")}
+                >
+                  Movie info
+                </Button>
+              </Card.Actions>
+              <Divider />
+              <Card.Title title={this.state.movie[2].title} />
+              <Card.Cover source={{ uri: this.state.movie[2].image }} />
+              <Card.Content>
+                <Paragraph>{this.state.movie[2].plot}</Paragraph>
+              </Card.Content>
+              <Card.Actions>
+                <Button
+                  onPress={() => this.props.navigation.navigate("Movie2Av")}
+                >
+                  Movie info
+                </Button>
+              </Card.Actions>
+            </Card>
           </ScrollView>
-          )}
-        
+        )}
       </View>
     );
   }
 }
 
-export default withNavigation(HomeMovies)
+export default withNavigation(HomeMovies);
--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,12 +1,62 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import axios from "axios";
 
-export default class UserScreen extends Component {
+import firebase from "../config/firebase";
+
+class UserScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: null,
+      curUserUid: firebase.auth().currentUser.uid,
+      movies: null
+    };
+  }
+
+  componentDidMount() {
+    const reservs = [];
+    firebase
+      .firestore()
+      .collection("reservations")
+      .get()
+      .then(querySnapshot => {
+        console.log(querySnapshot);
+        querySnapshot.docs.forEach(doc => {
+          reservs.push(doc.data());
+        });
+        this.setState({ reservations: reservs });
+        console.log(this.state.reservations[0].userSeats);
+        const markers = [];
+        firebase
+          .firestore()
+          .collection("films")
+          .get()
+          .then(querySnapshot => {
+            console.log(querySnapshot);
+            querySnapshot.docs.forEach(doc => {
+              markers.push(doc.data());
+            });
+            this.setState({ movie: markers });
+            console.log(this.state.movie);
+          });
+      });
+  }
+
   render() {
+    console.log(this.state.curUserUid);
     return (
-      <View>
-        <Text> UserScreen </Text>
+      <View style={{ alignSelf: "stretch" }}>
+        {this.state.reservations != null
+          ? this.state.reservations.map(reserv => {
+              this.state.curUserUid === reserv.userUid ? (
+                <Text>{reserv.movieID}</Text>
+              ) : null;
+            })
+          : null}
       </View>
     );
   }
 }
+
+export default UserScreen;
